d27975f518a8dc7c3b0705b1f7253e66decdb89a
Abdul456333 <mohammedabdul.cs23@bmsce.ac.in>
2024-10-21 14:41:40 +0530

    Add files via upload

---
+++ Areademo.java
@@ -0,0 +1,49 @@
+import java.util.*;
+abstract class Shape{
+    double i;
+    double j;
+    abstract void area();
+	Shape(double a,double b){
+	 i=a;
+         j=b;
+	}
+}
+
+class Rectangle extends Shape{
+	Rectangle(double a,double b){
+		super(a,b);
+	}
+	void area(){
+		System.out.println("area of rectangle is"+" "+i*j);
+	}
+}
+class Triangle extends Shape{
+	Triangle(double a,double b){
+		super(a,b);
+	}
+	void area(){
+		System.out.println("area of triangle is"+" "+0.5*i*j);
+	}
+}
+class Circle extends Shape{
+	Circle(double a,double b){
+		super(a,b);
+	}
+	void area(){
+		System.out.println("area of circle is"+" "+3.14*i*j);
+	}
+}
+class Main{
+	public static void main(String args[]){
+		Rectangle r1=new Rectangle(10.0,20.0);
+		Triangle t1=new Triangle(10.0,20.0);
+		Circle c1=new Circle(10.0,20.0);
+		Shape r;
+		r=r1;
+		r.area();
+		r=t1;
+		r.area();
+		r=c1;
+		r.area();
+	}
+}
