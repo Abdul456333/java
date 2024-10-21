ad6e15fa0102bbc318af2fb60bc630aa149c76db
Abdul456333 <mohammedabdul.cs23@bmsce.ac.in>
2024-10-21 14:58:47 +0530

    Update Areademo.java

--- Areademo.java
+++ Areademo.java
@@ -33,11 +33,11 @@ class Circle extends Shape{
 		System.out.println("area of circle is"+" "+3.14*i*j);
 	}
 }
-class Main{
+class Areademo{
 	public static void main(String args[]){
 		Rectangle r1=new Rectangle(10.0,20.0);
 		Triangle t1=new Triangle(10.0,20.0);
-		Circle c1=new Circle(10.0,20.0);
+		Circle c1=new Circle(10.0,10.0);
 		Shape r;
 		r=r1;
 		r.area();
